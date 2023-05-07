42b926b644cd0ba27b04d24afef0152374a31578
evan <chanjungskim@gmail.com>
2023-05-08 00:40:59 +0900

    Fix: .java to .dart in week4 - getx

--- week04/chan/StateManagement/lib/getx/get_x.dart
+++ week04/chan/StateManagement/lib/getx/get_x.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const CounterApp());
+  runApp(CounterApp());
 }
 
 class CounterController extends GetxController {
