2c59521e7a3f4fcd38a6b111a48abb698625ae29
evan <chanjungskim@gmail.com>
2023-05-08 01:15:31 +0900

    Fix: mobX runnable with generated file(counter.g.dart)

--- week04/chan/StateManagement/lib/mobx/counter.dart
+++ week04/chan/StateManagement/lib/mobx/counter.dart
@@ -1,6 +1,6 @@
 import 'package:mobx/mobx.dart';
 
-// 빌드 후 생성되는 파일.
+// 빌드 후 생성되는 파일을 가져온다. (flutter packages pub run build_runner watch)
 part 'counter.g.dart';
 
 // This is the class used by rest of your codebase
