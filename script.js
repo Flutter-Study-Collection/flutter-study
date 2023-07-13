00eb0cdde860a9810044a44c29cfd2d68126efc2
evan <chanjungskim@gmail.com>
2023-07-14 01:00:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ async function run() {
     };
 
     const chartUrl = 'https://quickchart.io/chart?c=' + encodeURIComponent(JSON.stringify(chartData));
-    const chartUrl = 'https://quickchart.io/chart?c=' + encodeURIComponent(JSON.stringify(chartData));
     fs.writeFileSync(process.env.GITHUB_ENV, `URL=${chartUrl}\n`, {flag: 'a'});
 }
 
