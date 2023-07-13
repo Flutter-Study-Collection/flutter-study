313613c178e6abdae9f776e06acb7c1944ebc938
evan <chanjungskim@gmail.com>
2023-07-13 23:55:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,32 @@
-const { createCanvas, loadImage } = require('canvas');
-const Chart = require('chart.js');
 const axios = require('axios');
 const moment = require('moment');
 
-async function getCommits(owner, repo, since, until) {
-    const response = await axios.get(`https://api.github.com/repos/${owner}/${repo}/commits?since=${since}&until=${until}`);
-    return response.data;
-}
+const since = moment().day(6).hour(12).minute(0).second(0).subtract(2, 'weeks').toISOString();
+const until = moment().day(6).hour(12).minute(0).second(0).toISOString();
 
-function processData(commits) {
-    const data = {};
+axios.get('https://api.github.com/repos/Flutter-Study-Collection/flutter-study/commits?since=' + since + '&until=' + until)
+  .then(response => {
+    const authors = {};
 
-    for (const commit of commits) {
-        const date = commit.commit.author.date;
-        const day = moment(date).format('YYYY-MM-DD');
-        data[day] = (data[day] || 0) + 1;
-    }
+    response.data.forEach(commit => {
+      const author = commit.commit.author.name;
+      authors[author] = (authors[author] || 0) + 1;
+    });
 
-    return {
-        labels: Object.keys(data),
+    const chartData = {
+      type: 'bar',
+      data: {
+        labels: Object.keys(authors),
         datasets: [{
-            data: Object.values(data)
+          label: 'Commits',
+          data: Object.values(authors)
         }]
+      }
     };
-}
 
-async function createChart(data) {
-    const canvas = createCanvas(800, 600);
-    const ctx = canvas.getContext('2d');
-    new Chart(ctx, {
-        type: 'line',
-        data,
-        options: {
-            scales: {
-                x: {
-                    type: 'time',
-                    time: {
-                        unit: 'day'
-                    }
-                }
-            }
-        }
-    });
-    await loadImage(canvas.toDataURL()).then((image) => {
-        const out = createCanvas(800, 600);
-        const ctx = out.getContext('2d');
-        ctx.drawImage(image, 0, 0);
-        const buffer = out.toBuffer('image/png');
-        require('fs').writeFileSync('chart.png', buffer);
-    });
-}
-
-async function run() {
-    const now = new Date();
-    now.setHours(12, 0, 0, 0);
-    const daysUntilSaturday = (7 - now.getDay() + 6) % 7;
-    const since = new Date(now.getTime() + daysUntilSaturday * 24 * 60 * 60 * 1000);
-    const until = new Date(since.getTime() + 14 * 24 * 60 * 60 * 1000);
-    const commits = await getCommits('Flutter-Study-Collection', 'flutter-study', since.toISOString(), until.toISOString());
-    const data = processData(commits);
-    await createChart(data);
-}
-
-run();
-
+    const chartUrl = 'https://quickchart.io/chart?c=' + encodeURIComponent(JSON.stringify(chartData));
+    console.log(chartUrl);
+  })
+  .catch(error => {
+    console.error(error);
+  });
