8aec165ab9057317f786ffe54db06c1f0bc01500
evan <chanjungskim@gmail.com>
2023-08-11 23:01:44 +0900

    Add: riverpod project

---
+++ week12/chan/riverpod_2_0/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_2_0/pages/after_using_hook_consumer_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ week12/chan/riverpod_2_0/lib/pages/after_using_hook_consumer_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// 별도의 State 클래스를 생성할 필요가 없습니다. 하나의 Page 클래스에서 모든 것을 처리할 수 있습니다.
+class MyHomePage extends HookConsumerWidget {
+  final String title;
+
+  MyHomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) { // WidgetRef 추가적으로 사용합니다.
+    // 데이터를 유지하기 위해 멤버필드나 provider를 사용할 필요없이 useState를 사용하면 데이터가 유지됩니다.
+    // 내부적으로는 ValueNotifier를 사용합니다.
+    var counter = useState(0);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '${counter.value}', // ValueNotifier이기 때문에 value로 데이터를 가져옵니다.
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => counter.value++, // ValueNotifier이기 때문에 value로 데이터를 변경합니다.
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
